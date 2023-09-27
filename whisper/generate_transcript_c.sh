a815a0ad30e606bdbceb77ddc337f0fc6570b8f5
Faran Khalid <faran.khalid@conovoinc.com>
2023-09-27 21:32:47 +0500

    syntax error on purpose

--- whisper/generate_transcript_c.sh
+++ whisper/generate_transcript_c.sh
@@ -48,7 +48,7 @@ if [ "$env" = "prod" ]; then
       --model_directory "$model_dir" \
       --model "$model" \
       --task "$job" \
-      --device "$device" \
+      --dece "$device" \
       --output_dir "$output_dir" \
       --threads "$threads" \
       --local_files_only True
